9be090e2e0651730f35013a9553f5a44cf83378a
Thiago Leite <thiagofmleite@gmail.com>
2024-05-23 18:17:21 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
+
+  // return transaction
+
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Server is running on port 3333')
+  })
