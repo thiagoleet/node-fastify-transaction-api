a64d3230e3805ba80e638f8f75a9f7654b1a50ae
Thiago Leite <thiagofmleite@gmail.com>
2024-05-23 19:03:01 -0300

    Adicionando cookies à aplicação

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,8 +4,16 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select('*')
+  app.get('/', async (request, reply) => {
+    const sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      return reply.status(401).send({ message: 'Unauthorized' })
+    }
+
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select('*')
 
     return {
       total: transactions.length,
@@ -47,12 +55,23 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     const id = randomUUID()
 
     await knex('transactions').insert({
       id,
       title,
       amount: type === 'credit' ? amount : -amount,
+      session_id: sessionId,
     })
 
     return reply.status(201).send({ message: 'Transaction created', id })
