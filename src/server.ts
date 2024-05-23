4f36674899a9c9da993633b18944dc38a150512e
Thiago Leite <thiagofmleite@gmail.com>
2024-05-23 19:15:02 -0300

    Criando um hook

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
+app.addHook('preHandler', async (request) => {
+  console.log(`[${request.method}] ${request.url}`)
+})
+
 app.register(cookie)
 app.register(transactionsRoutes, {
   prefix: '/transactions',
